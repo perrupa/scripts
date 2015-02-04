d96a616344f8e8457ebed87c93df0b9ffda8dde7
Chris Marlow <chris.marlow@achievers.com>
2015-02-04 11:11:08 -0500

    Got script echoing jiras and commit IDs (excluding merge commits)

--- get-diff.sh
+++ get-diff.sh
@@ -1,2 +1,25 @@
 #!/bin/bash
+code_directory="/dev-git/platform-a/"
 
+branch_base="develop"
+branch_candidate="HEAD"
+
+function get_revisions()
+{
+	log=""
+	(
+		cd $code_directory
+
+		# git log "$branch_base".."$branch_candidate"
+		$log=`git log "$branch_base".."$branch_candidate"`
+		# commits=`$log | awk "{print $1}"`
+		# subjects=`$log | awk "{print $2}"`
+
+	)
+	for entry in $log; 
+	do
+		 echo $entry 
+	done
+}
+
+get_revisions $branch_base $branch_candidate
--- test.sh
+++ test.sh
@@ -2,6 +2,10 @@
 base_branch="develop"
 candidate_branch=$1
 
+function get_jiras {
+	jira_pattern="[A-z]\{1,\}\-[0-9]\{1,\}"
+	grep "$jira_pattern"
+}
 
 function pfa {
 	pfa_directory="/dev-git/platform-a/"
@@ -10,7 +14,19 @@ function pfa {
 	# Enter PFA scope to execute command
 	(
 		cd $pfa_directory
-		$command
+		log=$command
+
+		echo "Commits:"
+		$log | awk '{print "   "$1}'
+
+		echo ""
+
+		echo "Jiras:"
+		$log | awk '{print "   "$2}' | get_jiras
+
+		echo ""
+
+		# echo "Files Changed"
 	)
 }
 
