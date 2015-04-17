736f74783f84ba152d5cdd98ff90eab142e39750
Chris Marlow <chris.marlow@achievers.com>
2015-04-17 15:19:51 -0400

    updated the shebang

--- analyze_branch.sh
+++ analyze_branch.sh
@@ -1,4 +1,4 @@
-#!bin/sh
+#!/bin/sh
 
 function get_jiras {
 	jira_pattern="[A-z]\{1,\}\-[0-9]\{1,\}"
@@ -21,17 +21,17 @@ base_branch="develop"
 candidate_branch=$1
 range="$base_branch..$candidate_branch"
 
-log=$(pfa "git log $range --oneline") 
-diff=$(pfa "git diff $range --numstat") 
+log=$(pfa "git log $range --oneline")
+diff=$(pfa "git diff $range --numstat")
 
 echo "Commits:"
-printf %s "$log" | 
+printf %s "$log" |
 	awk '{print "   " $1}'
 
 echo ""
 
 echo "Jiras:"
-printf %s "$log" | 
+printf %s "$log" |
 	awk '{print "   " $2}' | get_jiras
 
 echo ""
